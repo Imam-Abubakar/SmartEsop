668938c2f49dee5e34608671716db20fdabc370e
Imam Abubakar <imamabubakar2002@gmail.com>
2023-06-25 01:08:55 +0000

    (update): Developed the smart contract

--- EmployeeStockOptionPlan.sol
+++ EmployeeStockOptionPlan.sol
@@ -1,21 +1,218 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
+/**
+ * @title Employee Stock Option Plan
+ * @dev A smart contract to manage an Employee Stock Option Plan (ESOP) on the Ethereum blockchain.
+ */
 contract EmployeeStockOptionPlan {
-    // Define the necessary data structures and variables
-    
-    // Implement the necessary events
-    
-    // Implement the constructor
-    
-    // Implement the functions for granting stock options
-    
-    // Implement the functions for setting the vesting schedule
-    
-    // Implement the functions for exercising options
-    
-    // Implement the functions for tracking vested and exercised options
-    
-    // Implement the necessary modifiers and access control
-    
-    // Add any additional functions or modifiers as needed
+    address private owner; // Contract owner
+
+    struct Employee {
+        uint256 totalOptions; // Total granted options
+        uint256 vestedOptions; // Vested options
+        uint256 exercisedOptions; // Exercised options
+        uint256 vestingStartTime; // Vesting start time
+        uint256 vestingEndTime; // Vesting end time
+    }
+
+    mapping(address => Employee) private employees; // Mapping of employee addresses to Employee struct
+
+    event StockOptionsGranted(address indexed employee, uint256 options);
+    event VestingScheduleSet(
+        address indexed employee,
+        uint256 vestingStartTime,
+        uint256 vestingEndTime
+    );
+    event OptionsExercised(address indexed employee, uint256 options);
+
+    /**
+     * @dev Constructor function that sets the contract owner as the deployer of the contract.
+     */
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Modifier that allows only the contract owner to perform certain actions.
+     */
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only the contract owner can perform this action."
+        );
+        _;
+    }
+
+    /**
+     * @dev Modifier that allows only authorized employees to perform certain actions.
+     */
+    modifier onlyEmployee() {
+        require(
+            employees[msg.sender].totalOptions > 0,
+            "Only authorized employees can perform this action."
+        );
+        _;
+    }
+
+    /**
+     * @dev Grants stock options to an employee.
+     * @param employee The address of the employee.
+     * @param options The number of options to be granted.
+     * Requirements:
+     * - The number of options must be greater than zero.
+     */
+    function grantStockOptions(address employee, uint256 options)
+        public
+        onlyOwner
+    {
+        require(options > 0, "Number of options must be greater than zero.");
+
+        employees[employee].totalOptions += options;
+
+        emit StockOptionsGranted(employee, options);
+    }
+
+    /**
+     * @dev Sets the vesting schedule for an employee's options.
+     * @param employee The address of the employee.
+     * @param vestingStartTime The start time of the vesting period.
+     * @param vestingEndTime The end time of the vesting period.
+     * Requirements:
+     * - The vesting start time must be before the vesting end time.
+     * - The employee must have been granted options.
+     */
+    function setVestingSchedule(
+        address employee,
+        uint256 vestingStartTime,
+        uint256 vestingEndTime
+    ) public onlyOwner {
+        require(vestingStartTime < vestingEndTime, "Invalid vesting schedule.");
+        require(
+            employees[employee].totalOptions > 0,
+            "Employee has no granted options."
+        );
+        employees[employee].vestingStartTime = vestingStartTime;
+        employees[employee].vestingEndTime = vestingEndTime;
+        emit VestingScheduleSet(employee, vestingStartTime, vestingEndTime);
+    }
+
+    /**
+     * @dev Allows an employee to exercise their vested options.
+     * @param options The number of options to exercise.
+     * Requirements:
+     * - The number of options to exercise must be greater than zero.
+     * - The employee must have enough vested options.
+     * - The total number of exercised options must not exceed the total granted options.
+     */
+    function exerciseOptions(uint256 options) public onlyEmployee {
+        require(
+            options > 0,
+            "Number of options to exercise must be greater than zero."
+        );
+        require(
+            employees[msg.sender].vestedOptions >= options,
+            "Not enough vested options."
+        );
+        require(
+            employees[msg.sender].exercisedOptions + options <=
+                employees[msg.sender].totalOptions,
+            "Exceeded total granted options."
+        );
+
+        employees[msg.sender].vestedOptions -= options;
+        employees[msg.sender].exercisedOptions += options;
+
+        emit OptionsExercised(msg.sender, options);
+    }
+
+    /**
+     * @dev Retrieves the number of vested options for an employee.
+     * @param employee The address of the employee.
+     * @return The number of vested options.
+     */
+    function getVestedOptions(address employee) public view returns (uint256) {
+        return employees[employee].vestedOptions;
+    }
+
+    /**
+     * @dev Retrieves the number of exercised options for an employee.
+     * @param employee The address of the employee.
+     * @return The number of exercised options.
+     */
+    function getExercisedOptions(address employee)
+        public
+        view
+        returns (uint256)
+    {
+        return employees[employee].exercisedOptions;
+    }
+
+    /**
+     * @dev Allows an employee to transfer their vested options to another eligible employee.
+     * @param to The address of the recipient employee.
+     * @param options The number of options to transfer.
+     * Requirements:
+     * - The number of options to transfer must be greater than zero.
+     * - The recipient address must not be the same as the sender's address.
+     * - The recipient must be an eligible employee with granted options.
+     */
+    function transferOptions(address to, uint256 options) public onlyEmployee {
+        require(
+            options > 0,
+            "Number of options to transfer must be greater than zero."
+        );
+        require(to != msg.sender, "Cannot transfer options to oneself.");
+        require(
+            employees[to].totalOptions > 0,
+            "Recipient is not an eligible employee."
+        );
+
+        employees[msg.sender].vestedOptions -= options;
+        employees[to].totalOptions += options;
+
+        emit StockOptionsGranted(to, options);
+    }
+
+    /**
+     * @dev Updates the vested options for an employee after the vesting period has ended.
+     *      Only the contract owner can call this function.
+     * @param employee The address of the employee to update vested options for.
+     * @return A boolean indicating whether the vested options were successfully updated.
+     * @notice This function should be called by the contract owner to update the vested options for an employee
+     *         after the vesting period has ended.
+     * @dev Access Control:
+     *      - Only the contract owner can call this function.
+     *      - The vesting period must have ended for the employee.
+     */
+    function updateVestedOptions(address employee)
+        external
+        onlyOwner
+        returns (bool)
+    {
+        require(
+            block.timestamp >= employees[employee].vestingEndTime,
+            "Vesting period not yet ended."
+        );
+
+        employees[employee].vestedOptions = employees[employee].totalOptions;
+
+        return true;
+    }
+
+    /**
+     * @dev Retrieves the start and end dates of the vesting period for an employee.
+     * @param employee The address of the employee.
+     * @return A tuple containing the start and end dates of the vesting period.
+     */
+    function getVestingSchedule(address employee)
+        public
+        view
+        returns (uint256, uint256)
+    {
+        return (
+            employees[employee].vestingStartTime,
+            employees[employee].vestingEndTime
+        );
+    }
 }
--- README.md
+++ README.md
@@ -1,69 +1,41 @@
-# Employee Stock Option Plan Smart Contract Challenge
+# Employee Stock Option Plan Solution
 
-## Task
-You are tasked with designing and implementing a smart contract for an Employee Stock Option Plan (ESOP) on the Ethereum blockchain. The ESOP allows a company to grant stock options to its employees, define vesting schedules, and enable employees to exercise their vested options. Your goal is to create a secure, efficient, and user-friendly smart contract, named `EmployeeStockOptionPlan`, that manages the ESOP.
+A smart contract to manage an Employee Stock Option Plan (ESOP) on the Ethereum blockchain.
 
-## Smart Contract Specifications
-- Smart Contract Language: Solidity (version 0.8.0 or higher)
-- Blockchain Platform: Ethereum
+## Deployment
 
-## Employee Stock Option Plan Functionality
-Implement the following functionalities in the `EmployeeStockOptionPlan` smart contract:
+The contract has been deployed to goerli testnet for testing purposes. You can access the deployed contract at the following address:
 
-### 3.1. Granting Stock Options
-- Implement a function, `grantStockOptions`, that allows the company (contract owner) to grant stock options to an employee by specifying their address and the number of options.
-- Emit an event, `StockOptionsGranted`, to log the grant of stock options.
+Testnet Contract Address: [Contract Address](https://goerli.etherscan.io/address/0x3126def28532453846ce0d205e7f1eca9fef3fc0)
 
-### 3.2. Vesting Schedule
-- Implement a function, `setVestingSchedule`, that allows the company to set the vesting schedule for an employee's options.
 
-### 3.3. Exercising Options
-- Implement a function, `exerciseOptions`, that allows an employee to exercise their vested options.
+## Contract Details
 
-### 3.4. Tracking Vested and Exercised Options
-- Implement functions, `getVestedOptions` and `getExercisedOptions`, to retrieve the number of vested and exercised options for an employee.
+The Employee Stock Option Plan contract allows for the following functionality:
 
-### 3.5. Security and Permissions
-- Implement appropriate access control to ensure that only authorized employees can exercise their options.
+- Granting stock options to employees
+- Setting the vesting schedule for employee options
+- Allowing employees to exercise their vested options
+- Transferring vested options between employees
+- Updating vested options after the vesting period
 
-### 3.6. Ownership and Transferability
-- Implement a function, `transferOptions`, that allows employees to transfer their vested options to other eligible employees, subject to any transfer restrictions specified in the vesting schedule.
+## Contract Functions
 
-## Your Task
-Design and implement the `EmployeeStockOptionPlan` smart contract based on the provided skeleton code in the file `EmployeeStockOptionPlan.sol`. Your implementation should fulfill the following requirements:
+The contract exposes the following functions:
 
-### 4.1. Functionality
-The smart contract should correctly handle the granting of stock options, setting of vesting schedules, exercising of options, and tracking of vested and exercised options.
+- `grantStockOptions`: Grants stock options to an employee.
+- `setVestingSchedule`: Sets the vesting schedule for an employee's options.
+- `exerciseOptions`: Allows an employee to exercise their vested options.
+- `getVestedOptions`: Retrieves the number of vested options for an employee.
+- `getExercisedOptions`: Retrieves the number of exercised options for an employee.
+- `transferOptions`: Allows an employee to transfer their vested options to another eligible employee.
+- `updateVestedOptions`: Updates the vested options for an employee after the vesting period has ended.
+- `getVestingSchedule`: Retrieves the start and end dates of the vesting period for an employee.
 
-### 4.2. Security
-Ensure the smart contract follows best practices for security, including protection against common attacks like reentrancy, overflow/underflow, and unauthorized access.
+For detailed information on each function, including parameters and usage, please refer to the contract's source code.
 
-### 4.3. Documentation
-Include comprehensive documentation that explains the design decisions, contract architecture, and usage instructions. Code comments should be provided where necessary to improve code readability and maintainability.
+## Access Control
 
-Use the provided skeleton code in the file `EmployeeStockOptionPlan.sol` as a starting point for your implementation. Feel free to modify and expand the code as needed to fulfill the requirements.
+The contract implements access control through modifiers. The `onlyOwner` modifier restricts certain functions to be called only by the contract owner. The `onlyEmployee` modifier ensures that specific actions can only be performed by authorized employees.
 
-## Evaluation Criteria
-Your solution will be evaluated based on the following criteria:
 
-### 5.1. Correctness
-The smart contract should implement the specified functionality accurately and without any critical bugs.
-
-### 5.2. Documentation
-Include comprehensive documentation that explains the design decisions, contract architecture, and usage instructions. Code comments should be provided where necessary to improve code readability and maintainability.
-
-Please note that the provided skeleton code is a starting point and may require further modifications and additions based on your implementation strategy and requirements.
-
-## Submission Guidelines
-Please follow the below guidelines for your submission:
-
-1. Fork this repository.
-2. Create a new branch for your solution.
-3. Implement your solution in the file `EmployeeStockOptionPlan.sol`.
-4. Provide the required documentation and instructions.
-5. Commit and push your changes to your forked repository.
-6. Submit a pull request (PR) to this repository.
-
-Please make sure to include all the necessary files and information in your submission.
-
-Good luck with your implementation! If you have any questions, feel free to ask.
